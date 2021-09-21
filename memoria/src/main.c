fb87dd911a543e7d02c1e75cb7a51c7d11aa3fd0
mabelenda <martin.abelenda98@gmail.com>
2021-09-21 20:23:37 -0300

    start memory server

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -1,10 +1,21 @@
 #include "main.h"
 int main(int argc, char ** argv){
-    if(argc > 1 && strcmp(argv[1],"-test")==0)
+    if(argc > 1 && strcmp(argv[1],"-test") == 0){
         return run_tests();
-    else{  
-        t_log* logger = log_create("./cfg/memoria.log", "MEMORIA", true, LOG_LEVEL_INFO);
-        log_info(logger, "Soy MEMORIA! %s", mi_funcion_compartida());
-        log_destroy(logger);
-    } 
+    }
+
+    logger = log_create(LOG_PATH, PROGRAM, true, LOG_LEVEL_INFO);
+
+    config = config_create(CONFIG_PATH);
+    
+    _start_server(config_get_string_value(config, PORT_CONFIG), handler, logger);
+}
+
+void handler(int fd, char* id, int opcode, void* payload, t_log* logger){
+    log_info(logger, "Recibí un mensaje");
+}
+
+void free_memory(){
+    log_destroy(logger);  
+    config_destroy(config);  
 }
