a912f63e8b86e793f48cc002844c11364baf5ab7
mabelenda <martin.abelenda98@gmail.com>
2021-10-23 22:32:00 -0300

    Fix swamp init

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -12,7 +12,7 @@ int main(int argc, char ** argv){
     _start_server(config_get_string_value(config, PORT_CONFIG), handler, logger);
 }
 
-void init_swamp_connection(char* ip, char* port){
+void init_swamp_connection(){
     swamp_fd = _connect(config_get_string_value(config, SWAMP_IP), config_get_string_value(config, SWAMP_PORT), logger);
     pthread_mutex_init(&swamp_mutex, NULL);
 }
