f639ca9dee3a98eb82b3b180e7d035c64c5cfb2c
marianopoll <mariampoll98@gmail.com>
2021-11-26 23:16:24 -0300

    correcciones swamp

--- mate-lib/main.c
+++ mate-lib/main.c
@@ -1,33 +1,48 @@
+/*
+ ============================================================================
+ Name        : PruebaSuspension.c
+ Description : Prueba de suspension de carpinchos del TP CarpinchOS
+ ============================================================================
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <pthread.h>
 #include <matelib.h>
+#include <string.h>
 
-void* io_thread(void* mate_ref) {
-    mate_sem_wait(mate_ref, "SEM1");
-    printf("And this one second...\n");
-
-    // Memory tooling usage
-    mate_pointer ptr = mate_memalloc(mate_ref, sizeof(char) * 8);
-    mate_memwrite(mate_ref, "PRINTER", ptr, sizeof(char) * 8);
-    char *io_type = malloc(sizeof(char) * 8);
-    mate_memread(mate_ref, ptr, io_type, sizeof(char) * 8);
-
-    // IO Usage
-    mate_call_io(mate_ref, io_type, "I'm content to print...");
-
-    // Freeing Memory
-    // Closing Lib
-    free(io_type);
-    mate_memfree(mate_ref, ptr);
-
-    return 0;
-}
+#define SEMAFORO_SALUDO "SEM_HELLO"
 
 int main(int argc, char *argv[]) {
-    // Lib instantiation
-    mate_instance mate_ref;
-    mate_init(&mate_ref, "./cfg/mate-lib.conf");
+
+    if(argc < 2){
+        printf("No se ingresó archivo de configuración");
+        exit(EXIT_FAILURE);
+    }
+
+    char* config = argv[1];
+
+	printf("MAIN - Utilizando el archivo de config: %s\n", config);
+
+	mate_instance instance;
+
+	mate_init(&instance, (char*)config);
+
+    char saludo[] = "No, ¡hola humedal!\n";
+
+    mate_pointer saludoRef = mate_memalloc(&instance, strlen(saludo));
+
+    mate_memwrite(&instance, saludo, saludoRef, strlen(saludo));
+
+    mate_sem_init(&instance, SEMAFORO_SALUDO, 0);
+
+    mate_sem_wait(&instance, SEMAFORO_SALUDO);
+
+    mate_memread(&instance, saludoRef, saludo, strlen(saludo));
+
+    printf(saludo);
+
+    mate_close(&instance);
+
+	return EXIT_SUCCESS;
 }
