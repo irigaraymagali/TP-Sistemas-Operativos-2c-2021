7978aae208e8fdb6448f4800b709a451a005c57c
mabelenda <martin.abelenda98@gmail.com>
2021-12-17 18:57:09 -0300

    add deploy commons and clone pruebas

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-length=$(($#-1))
-OPTIONS=${@:1:$length}
-REPONAME="${!#}"
 CWD=$PWD
-echo -e "\n\nInstalling commons libraries...\n\n"
+echo -e "\n\nInstalando commons Library, o eso creo...\n\n"
 COMMONS="so-commons-library"
 git clone "https://github.com/sisoputnfrba/${COMMONS}.git" $COMMONS
 cd $COMMONS
+
 sudo make uninstall
 make all
 sudo make install
-cd $CWD
-echo -e "\n\nBuilding projects...\n\n"
-make -C ./proceso1
-make -C ./proceso2
-make -C ./proceso3
-echo -e "\n\nDeploy done!\n\n"
+ls -a
+
+cd ..
+PRUEBACARPINCHO="carpinchos-pruebas"
+echo -e "\n\n Instalando las pruebas de carpinchoide... \n\n"
+git clone "https://github.com/sisoputnfrba/carpinchos-pruebas.git" $PRUEBACARPINCHO
